c89766b1f5b6f2f8a66243952604a9f13c485522
kyotsuetsu <quyuening0510@gmail.com>
2024-12-10 20:31:45 +0900

    changed styles on hotel search page

--- resources/views/customers/hotel_search.blade.php
+++ resources/views/customers/hotel_search.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.customer')
 
 @section('title', 'Wheelchair and Senior')
 
@@ -17,8 +17,8 @@
             </div>
         </div>
         
-        <div class="d-flex justify-content-between mb-3">
-            <div>
+        <div class="d-flex justify-content-start mb-3">
+            <div class="me-3">
                 <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Filter
                 </button>
@@ -27,7 +27,7 @@
                     <a class="dropdown-item" href="#">Option 2</a>
                 </div>
             </div>
-            <div>
+            <div class="me-3">
                 <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Price
                 </button>
@@ -36,7 +36,7 @@
                     <a class="dropdown-item" href="#">High to Low</a>
                 </div>
             </div>
-            <div>
+            <div class="me-3">
                 <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Room
                 </button>
@@ -125,7 +125,7 @@
     </div>
 @endsection
 
-@push('styles')
+{{-- @push('styles')
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <style>
@@ -139,4 +139,4 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-@endpush
+@endpush --}}
