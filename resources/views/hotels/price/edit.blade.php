999179a413ea1ebdb0dae6c36fffefb0249d42ea
watanabe shinji <>
2024-12-12 22:23:23 +0900

    update hotel price management

--- resources/views/hotels/price/edit.blade.php
+++ resources/views/hotels/price/edit.blade.php
@@ -3,21 +3,24 @@
 @section('content')
 
 <h1 class="mb-4">Edit Prices</h1>
+<div class="d-flex justify-content-between align-items-center mb-3">
+    <button class="btn btn-outline-secondary">&lt;</button>
+    <button class="btn btn-outline-secondary">&gt;</button>
+</div>
 <form action="#" method="POST">
     @csrf
     <table class="table table-bordered">
         <thead class="header-row">
             <tr>
-                <th>Room Type</th>
-            
-                    <th>11/1(mon)</th>
-                    <th>11/1(mon)</th>
-                    <th>11/1(mon)</th>
-                    <th>11/1(mon)</th>
-                    <th>11/1(mon)</th>
-                    <th>11/1(mon)</th>
-                    <th>11/1(mon)</th>
-            
+            <th class="text-start col-2">Room Type</th>
+            <th class="col-1">11/1 (Mon)</th>
+            <th class="col-1">11/2 (Tue)</th>
+            <th class="col-1">11/3 (Wed)</th>
+            <th class="col-1">11/4 (Thu)</th>
+            <th class="col-1">11/5 (Fri)</th>
+            <th class="bg-primary text-white col-1">11/6 (Sat)</th>
+            <th class="bg-danger text-white col-1">11/7 (Sun)</th>
+           
             </tr>
         </thead>
         <tbody>
@@ -25,33 +28,40 @@
                 <tr>
                     <td>Single Room</td>
                     
-                        <td>
+                    <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-75" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                    
                 </tr>
@@ -60,31 +70,38 @@
                     
                     <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                    
                 </tr>
@@ -93,33 +110,39 @@
                     
                     <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
                         <td>
                             <input type="number" name="" value="" 
-                                   class="form-control text-center" />
+                                   class="form-control text-center w-50" />
+                                   %
                         </td>
-                   
                 </tr>
           
         </tbody>
--- resources/views/hotels/price/show.blade.php
+++ resources/views/hotels/price/show.blade.php
@@ -3,18 +3,22 @@
 @section('content')
 
 <h1 class="mb-4">Price Management</h1>
+<div class="d-flex justify-content-between align-items-center mb-3">
+    <button class="btn btn-outline-secondary">&lt;</button>
+    <button class="btn btn-outline-secondary">&gt;</button>
+</div>
 <table class="table table-bordered">
     <thead class="header-row">
     <tr>
-            <th>Room Type</th>
-           
-                <th>11/1(mon)</th>
-                <th>11/1(mon)</th>
-                <th>11/1(mon)</th>
-                <th>11/1(mon)</th>
-                <th>11/1(mon)</th>
-                <th>11/1(mon)</th>
-                <th>11/1(mon)</th>
+
+            <th class="text-start col-2">Room Type</th>
+            <th class="col-1">11/1 (Mon)</th>
+            <th class="col-1">11/2 (Tue)</th>
+            <th class="col-1">11/3 (Wed)</th>
+            <th class="col-1">11/4 (Thu)</th>
+            <th class="col-1">11/5 (Fri)</th>
+            <th class="bg-primary text-white col-1">11/6 (Sat)</th>
+            <th class="bg-danger text-white col-1">11/7 (Sun)</th>
            
         </tr>
     </thead>
