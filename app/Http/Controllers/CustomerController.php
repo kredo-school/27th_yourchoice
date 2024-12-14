2a022f947ec0dfcca4770bcb82748711417e27df
Miu <meexxvz@gmail.com>
2024-12-14 18:56:18 +0900

    FE/auth/register_top- changed the controller for register_top
    changes:
    CustomerController.php
    HomeController.php
    web.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
+    public function register_top()
+    {
+        return view('auth.register_top');
+    }
     public function customerRegister()
     {
         return view('auth.customer_register');
--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,22 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/register_top', [App\Http\Controllers\HomeController::class, 'register'])->name('register_top');
-
-
-
+//MypageController
 Route::get('/mypage/reviewlist',[MypageController::class,'reviewlist'])->name('mypage.reviewlist');
 Route::get('/mypage/reviewlist2',[MypageController::class,'reviewlist2'])->name('mypage.reviewlist2');
 Route::get('/mypage/profile/show',[MypageController::class,'profileShow'])->name('mypage.profileShow');
 Route::get('/mypage/profile/edit',[MypageController::class,'profileEdit'])->name('mypage.profileEdit');
 Route::get('/mypage/profile/password',[MypageController::class,'profilePassword'])->name('mypage.profilePassword');
 
-
+//HotelController
 Route::get('/hotels', [HotelController::class, 'index']);
 Route::get('/hotel_register', [HotelController::class,'hotelRegister'])->name('hotelRegister');
 
+//HotelAdminContoller
 Route::get('/profile/show',[HotelAdminController::class,'profileshow'])->name('profile.show');
 Route::get('/profile/edit',[HotelAdminController::class,'profileedit'])->name('profile.edit');
+
+//CustomerController
+Route::get('/register_top', [CustomerController::class, 'register_top'])->name('register_top');
 
 Route::get('/customer_register', [CustomerController::class,'customerRegister'])->name('customerRegister');
