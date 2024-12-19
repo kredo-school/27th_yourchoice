3b4ca4091a669cf5be4666b13e335ca03adb598a
kyotsuetsu <quyuening0510@gmail.com>
2024-12-19 21:50:08 +0900

    Added Google map at hotel_search page

--- resources/views/customers/hotel_detail.blade.php
+++ resources/views/customers/hotel_detail.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'Hotel Detail')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/hotel_search.css') }}">
+<link rel="stylesheet" href="{{ asset('css/jquery-ui.css')}}">
+
+
 <div class="container mt-5">
     <div class="row">
         <!-- Large Image -->
@@ -55,12 +59,18 @@
                     <div class="mt-4">
                         <h6>Explore the area</h6>
                         <p>Blackheath, NSW</p>
-                        <a href="#">View map</a>
+                        {{-- <a href="#">View map</a> --}}
                     </div>
+                </div>
+            </div>
+            <div class="card mt-2">
+                <div class="card-body">
+                    <div id="map"></div>
                 </div>
             </div>
         </div>
     </div>
+
     <div class="input-group my-4 w-50">
         <input type="date" class="form-control">
         <input type="number" class="form-control" placeholder="Travellers">
@@ -117,5 +127,18 @@
         </div>
     </div>
 </div>
+
+<script>
+    (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+      key: "AIzaSyAQ1u-pft-pLmlVb5TYcprtivdXXNkjfDk",
+      v: "weekly",
+      // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+      // Add other bootstrap parameters as needed, using camel case.
+    });
+</script>
+
+<script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ asset('js/jquery-ui.js') }}"></script>
+<script src="{{ asset('js/jquery.js') }}"></script>
 
 @endsection
