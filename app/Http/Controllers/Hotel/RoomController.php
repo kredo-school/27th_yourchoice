2b47fe0b263afafd98b6136aa5367a5686e22f7b
Rina Koyasu <ailes8pri.n@gmail.com>
2024-12-21 11:47:03 +0900

    FE/hotels/rooms/rina changes:create, show, edit, delete page (modify the button links), web.php, RoomController

--- app/Http/Controllers/Hotel/RoomController.php
+++ app/Http/Controllers/Hotel/RoomController.php
@@ -7,18 +7,24 @@ use App\Http\Controllers\Controller;
 
 class RoomController extends Controller
 {
-    public function show()
-    {
-        return view('hotels.rooms.show');
-    }
+  public function show()
+  {
+    return view('hotels.rooms.show');
+  }
 
-    public function create()
-    {
-        return view('hotels.rooms.create');
-    }
+  public function create()
+  {
+    return view('hotels.rooms.create');
+  }
 
-    public function edit()
-    {
-        return view('hotels.rooms.edit');
-    }
+  public function edit()
+  {
+    return view('hotels.rooms.edit');
+  }
+
+  public function destroy()
+  {
+    // return view('hotels.rooms.modals.delete');後で消す
+    return redirect()->back();
+  }
 }
