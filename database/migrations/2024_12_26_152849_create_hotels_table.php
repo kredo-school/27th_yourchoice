2422763f38ded95bd6e7ded1c924bdaae0f88b35
Rina Koyasu <ailes8pri.n@gmail.com>
2024-12-27 23:19:28 +0900

    BE/hotels/profile/rina change: hotels.table

--- database/migrations/2024_12_26_152849_create_hotels_table.php
+++ database/migrations/2024_12_26_152849_create_hotels_table.php
@@ -12,28 +12,28 @@ return new class extends Migration
   public function up(): void
   {
     Schema::create('hotels', function (Blueprint $table) {
-      $table->id();
-      $table->unsignedBigInteger('user_id');
-      $table->string('hotel_name');//laravelのstringはvarcher(255)と同じ
-      $table->string('email')->unique();
-      $table->text('url'); // URLは長くなる可能性があるためtext
-      $table->string('postal_code');
-      $table->string('prefecture');
-      $table->string('city');
-      $table->string('address');
-      $table->string('phone_number');
-      $table->string('access');
-      $table->text('description');
-      $table->longText('image_main');
-      $table->longText('image_sub1')->nullable();
-      $table->longText('image_sub2')->nullable();
-      $table->longText('image_sub3')->nullable();
-      $table->longText('image_sub4')->nullable();
-      $table->unsignedInteger('cancellation_period'); // 正の整数のみ許可
-      $table->unsignedInteger('general_fee'); // 正の整数のみ許可
-      $table->unsignedInteger('sameday_fee'); // 正の整数のみ許可
-      $table->decimal('breakfast_price', 10, 2);
-      $table->timestamps();
+      $table->id(); // ホテルID
+      $table->unsignedBigInteger('user_id'); // ユーザーID（外部キー）
+      $table->string('hotel_name'); // ホテル名
+      $table->string('email')->unique(); // メール
+      $table->text('url'); // ホテルの公式ウェブサイトURL
+      $table->string('postal_code', 10); // 郵便番号
+      $table->string('prefecture', 10); // 都道府県
+      $table->string('city', 100); // 市
+      $table->string('address', 100); // 市以下の住所
+      $table->string('phone_number', 20); // 電話番号
+      $table->string('access'); // 最寄駅までのアクセス時間
+      $table->text('description'); // ホテルの説明
+      $table->longText('image_main'); // ホテルの画像(メイン)
+      $table->longText('image_sub1')->nullable(); // ホテルの画像(サブ1)
+      $table->longText('image_sub2')->nullable(); // ホテルの画像(サブ2)
+      $table->longText('image_sub3')->nullable(); // ホテルの画像(サブ3)
+      $table->longText('image_sub4')->nullable(); // ホテルの画像(サブ4)
+      $table->unsignedInteger('cancellation_period'); //キャンセル期限
+      $table->unsignedInteger('general_fee'); // 通常キャンセル(%)
+      $table->unsignedInteger('sameday_fee'); // 当日以降キャンセル(%)
+      $table->decimal('breakfast_price', 10, 2); // 朝食の値段
+      $table->timestamps(); // 作成・更新日時
 
       $table->foreign('user_id')->references('id')->on('users');
     });
