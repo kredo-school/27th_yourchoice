1ee641830c804f572ab2a17e3fe7ae39ccfc255d
watanabe shinji <>
2024-12-28 10:55:52 +0900

    create user category table create hotel category table

--- database/migrations/2024_12_27_132428_create_reservations_table.php
+++ database/migrations/2024_12_27_132428_create_reservations_table.php
@@ -26,7 +26,6 @@ return new class extends Migration
 
             // 外部キー制約
             $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
-            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
             $table->foreign('payment_id')->references('id')->on('payments')->onDelete('cascade');
         });
     }
