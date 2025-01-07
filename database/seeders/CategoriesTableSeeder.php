28fc5e620aa96d243f5cd3aab595133832d67a6d
Rina Koyasu <ailes8pri.n@gmail.com>
2025-01-07 22:00:22 +0900

    BE/hotels/profile/rina changes: UsersTable.seeder CategoriesTable.seeder

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+
 use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -8,42 +9,97 @@ use Illuminate\Support\Facades\DB;
 
 class CategoriesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        DB::table('categories')->insert([
-            [
-                'id' => '1',
-                'type' => 'category',
-                'name' => 'Wheelchair and Senior',
-            ],
-            [
-                'id' => '2',
-                'type' => 'category',
-                'name' => 'Pregnancy',
-            ],
-            [
-                'id' => '3',
-                'type' => 'category',
-                'name' => 'Family',
-            ],
-            [
-                'id' => '4',
-                'type' => 'category',
-                'name' => 'Visual and Hearing Impaired',
-            ],
-            [
-                'id' => '5',
-                'type' => 'category',
-                'name' => 'Religious',
-            ],
-            [
-                'id' => '6',
-                'type' => 'category',
-                'name' => 'English Friendly',
-            ],
-        ]);
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    DB::table('categories')->insert([
+      [
+        'id' => '1',
+        'type' => 'category',
+        'name' => 'Wheelchair and Senior',
+      ],
+      [
+        'id' => '2',
+        'type' => 'category',
+        'name' => 'Pregnancy',
+      ],
+      [
+        'id' => '3',
+        'type' => 'category',
+        'name' => 'Family',
+      ],
+      [
+        'id' => '4',
+        'type' => 'category',
+        'name' => 'Visual and Hearing Impaired',
+      ],
+      [
+        'id' => '5',
+        'type' => 'category',
+        'name' => 'Religious',
+      ],
+      [
+        'id' => '6',
+        'type' => 'category',
+        'name' => 'English Friendly',
+      ],
+      [
+        'id' => '7',
+        'type' => 'service',
+        'name' => 'Parking availability',
+      ],
+      [
+        'id' => '8',
+        'type' => 'service',
+        'name' => 'Luggage storage service',
+      ],
+      [
+        'id' => '9',
+        'type' => 'service',
+        'name' => 'Breakfast',
+      ],
+      [
+        'id' => '10',
+        'type' => 'amenity',
+        'name' => 'Wi-Fi',
+      ],
+      [
+        'id' => '11',
+        'type' => 'amenity',
+        'name' => 'Air conditioning',
+      ],
+      [
+        'id' => '12',
+        'type' => 'amenity',
+        'name' => 'TV',
+      ],
+      [
+        'id' => '13',
+        'type' => 'amenity',
+        'name' => 'Dryer',
+      ],
+      [
+        'id' => '14',
+        'type' => 'free_toiletries',
+        'name' => 'Shampoo',
+      ],
+      [
+        'id' => '15',
+        'type' => 'free_toiletries',
+        'name' => 'Conditioner',
+      ],
+      [
+        'id' => '16',
+        'type' => 'free_toiletries',
+        'name' => 'Body wash',
+      ],
+      [
+        'id' => '17',
+        'type' => 'free_toiletries',
+        'name' => 'Toothbrush&paste',
+      ],
+    ]);
+  }
 }
