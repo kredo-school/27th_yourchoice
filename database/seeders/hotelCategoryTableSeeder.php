33c383e78686c4af555fecebad20872f2d092af4
Miu <meexxvz@gmail.com>
2025-01-14 21:56:42 +0900

    additional rows for hotel category seeder

--- database/seeders/hotelCategoryTableSeeder.php
+++ database/seeders/hotelCategoryTableSeeder.php
@@ -63,22 +63,6 @@ class HotelCategoryTableSeeder extends Seeder
                 'hotel_id' => 6,
                 'category_id' => 5,
             ],
-            [
-                'hotel_id' => 2,
-                'category_id' => 14,
-            ],
-            [
-                'hotel_id' => 2,
-                'category_id' => 15,
-            ],
-            [
-                'hotel_id' => 2,
-                'category_id' => 16,
-            ],
-            [
-                'hotel_id' => 2,
-                'category_id' => 17,
-            ],
         ]);
     }
 }
